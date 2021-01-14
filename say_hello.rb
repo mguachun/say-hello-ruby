0b85a157470104622bd614598070d6d38ee2992b
Melissa Guachun <1212malka@gmail.com>
2021-01-13 23:43:15 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,4 @@
-def say_hello(name="Ruby programmer")
-    puts "Hello #{name}"
+def say_hello(name="Ruby Programmer")
+ puts "Hello #{name}!"
 end
-
-say_hello()
-
-say_hello("Gabriela")
-
-learn test
-
-
 
